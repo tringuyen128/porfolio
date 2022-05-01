c63705c7371e6e7609113a8e4b5d11467f46ecf1
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-05-01 08:37:36 -0700

    first commit

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './nav.css'
+
+const Nav = () => {
+  return <div>nav</div>
+}
+
+export default Nav
---
+++ src/components/projects/Projects.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './project.css'
+
+const Projects = () => {
+  return <div>projects</div>
+}
+
+export default Projects
